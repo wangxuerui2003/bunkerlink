89bbde856a2e667aa9a20ffa56f5038e34f5082e
Dun Yan <ong.dy2004@gmail.com>
2024-06-09 20:46:50 +0800

    dunyan: changed homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/bunker.png',
+                width: 100,
+                height: 100,
+              ),
+              const SizedBox(height: 20.0),              
+              const Text(
+                'Bunkerlink',
+                style: TextStyle(
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Find the nearest bunkers and get help!',
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              const SizedBox(height: 40.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your button functionality here
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(Colors.lightGreen), // Background color
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0), // Adjust the value as needed
+                    ),
+                  ),
+                ),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.start, // You can choose any icon you prefer
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 8.0),
+                    Text(
+                      'Get Started',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white, // Text color
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
