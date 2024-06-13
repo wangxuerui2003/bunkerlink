d7358ce6b3be58d761b90fafe5bd3d78e83f3611
Dun Yan <ong.dy2004@gmail.com>
2024-06-13 23:24:51 +0800

    dunyan: restructure the codebase

---
+++ lib/home.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class FrontScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/bunker.png',
+              width: 100,
+              height: 100,
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              'Bunkerlink',
+              style: TextStyle(
+                fontSize: 36.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              'Find the nearest bunkers and get help!',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 40.0),
+            SizedBox(
+              width: 200.0,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(Colors.lightGreen), 
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0), 
+                    ),
+                  ),
+                ),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.play_arrow, 
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 8.0),
+                    Text(
+                      'Get Started',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white, 
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            SizedBox(
+              width: 200.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your button later lol
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(Colors.green), 
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0), 
+                    ),
+                  ),
+                ),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.map, 
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 8.0),
+                    Text(
+                      'Map',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import "map.dart";
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,66 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/bunker.png',
-                width: 100,
-                height: 100,
-              ),
-              const SizedBox(height: 20.0),              
-              const Text(
-                'Bunkerlink',
-                style: TextStyle(
-                  fontSize: 36.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              const Text(
-                'Find the nearest bunkers and get help!',
-                style: TextStyle(
-                  fontSize: 18.0,
-                ),
-              ),
-              const SizedBox(height: 40.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your button functionality here
-                },
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.lightGreen), // Background color
-                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0), // Adjust the value as needed
-                    ),
-                  ),
-                ),
-                child: const Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.start, // You can choose any icon you prefer
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 8.0),
-                    Text(
-                      'Get Started',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white, // Text color
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: FrontScreen(),
     );
   }
 }
